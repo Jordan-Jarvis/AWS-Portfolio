ecffd57a88218e098c72f8b80a2bb5afb0342e2e
Jordan-Jarvis <jj.mm.jj@hotmail.com>
2021-02-05 13:42:24 -0700

    Modified Resume Pdf Viewer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,27 @@ import NavigationBar from './bootstraps/header'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react"
 import { PDFViewer } from 'react-view-pdf';
-function App() {
+import ExampleRouter from './Router';
+// function App() {
+//   const title = "Welcome to the new blo";
+//   const likes = 50;
+//   const person = { name: "yoshi", age: 30}
+//   const link = "http://www.google.com";
+//   return (
+//     <div className="App">
+//       <div className="stickyTop">
+//         <NavigationBar />
+//       </div>
+//       <PDFViewer url="Resume.pdf" />
+      
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+
+
+  render() {
   const title = "Welcome to the new blo";
   const likes = 50;
   const person = { name: "yoshi", age: 30}
@@ -16,11 +36,26 @@ function App() {
     <div className="App">
       <div className="stickyTop">
         <NavigationBar />
+        <Subclass property="string"/>
+        <ExampleRouter />
       </div>
       <PDFViewer url="Resume.pdf" />
       
     </div>
   );
+  }
+}
+
+class Subclass extends React.Component {
+
+  render() {
+    console.log(this.props.property);
+    return (
+    <div>
+      Hi
+    </div>
+    )
+  }
 }
 
 export default App;
