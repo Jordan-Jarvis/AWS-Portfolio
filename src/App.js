76c7016da107c3f009d8e97633e15c2f58a472a2
Jordan-Jarvis <jj.mm.jj@hotmail.com>
2021-02-05 14:04:46 -0700

    Added public bucket for file serving

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react"
 import { PDFViewer } from 'react-view-pdf';
 import ExampleRouter from './Router';
+import { Storage } from 'aws-amplify'
 // function App() {
 //   const title = "Welcome to the new blo";
 //   const likes = 50;
@@ -26,6 +27,29 @@ import ExampleRouter from './Router';
 
 class App extends React.Component {
 
+  state = {
+    files: [],
+    file: ""
+  }
+  componentDidMount() {
+    this.listFiles()
+  }
+  onChange(e) {
+    const file = e.target.files[0]
+    Storage.put(file.name, file)
+    .then (() => this.listFiles())
+    .catch(err => console.log(err));
+  }
+
+  listFiles = async () => {
+    const files = await Storage.list('')
+    this.setState({ files })
+  }
+
+  selectFile = async file => {
+    const signed = await Storage.get(file.key)
+    this.setState({ file: signed })
+  }
 
   render() {
   const title = "Welcome to the new blo";
@@ -38,6 +62,30 @@ class App extends React.Component {
         <NavigationBar />
         <Subclass property="string"/>
         <ExampleRouter />
+      </div>
+      <div>
+        <input
+          type="file" accept='image/png'
+          onChange={(e) => this.onChange(e)}
+        />
+        <button onClick={this.listFiles}>
+          List Files
+        </button>
+        <div>
+        {
+          this.state.files.map((file, i) => (
+           <p onClick={() => this.selectFile(file)}>{file.key}</p>
+          ))
+        }
+        </div>
+        {
+          this.state.file && (
+            <img
+              src={this.state.file}
+              style={{width: 300}}
+            />
+          )
+        }
       </div>
       <PDFViewer url="Resume.pdf" />
       
