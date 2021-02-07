0affb090b7f9562929a72937552ac54a4a7799a3
Jordan-Jarvis <jj.mm.jj@hotmail.com>
2021-02-06 17:20:53 -0700

    Updated readme and added content to descriptions

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -8,8 +8,28 @@ const ProjectList = () => (
 
 
 <div>
-        <ProjectCard title = 'TEST TEMPLATE' text = "This contains the description of the project" buttonText = "Link to Source"/>
-        <ProjectCard title = 'TEST TEMPLATE' text = "This contains the description of the project" buttonText = "Link to source" imgURL = 'https://raw.githubusercontent.com/Jordan-Jarvis/RPi-Diskalvier-network-player/master/static/screenshot.png' newURL = 'https://github.com/Jordan-Jarvis/RPi-Diskalvier-network-player'/>
+        <ProjectCard 
+                title = 'This Site' 
+                text = "This contains the source code for the site you are looking at now. It is built in Node.js, uses React for the front-end, and is hosted on AWS." 
+                buttonText = "Go to Source"
+                newURL = "https://github.com/Jordan-Jarvis/AWS-Portfolio"
+                />
+
+        <ProjectCard 
+                title = 'RPi Disklavier Network Player' 
+                text = "Will add a description soon. Right now the site is under construction." 
+                buttonText = "Link to source" 
+                imgURL = 'https://raw.githubusercontent.com/Jordan-Jarvis/RPi-Diskalvier-network-player/master/static/screenshot.png' 
+                newURL = 'https://github.com/Jordan-Jarvis/RPi-Diskalvier-network-player'
+                />
+        <ProjectCard 
+                title = 'TEST TEMPLATE' 
+                text = "This contains the description of the project" 
+                buttonText = "Link to source" 
+                imgURL = 'https://raw.githubusercontent.com/Jordan-Jarvis/RPi-Diskalvier-network-player/master/static/screenshot.png' 
+                newURL = 'https://github.com/Jordan-Jarvis/RPi-Diskalvier-network-player'
+                />
+
 </div>
       )
     
