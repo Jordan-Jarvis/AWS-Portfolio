dab8aa815ae66f95d1e6f715503d5e1e1faf276a
Jordan-Jarvis <32745130+Jordan-Jarvis@users.noreply.github.com>
2022-11-18 14:10:51 -0700

    Adding redirect for wedding

--- src/views/wedding/wedding.js
+++ src/views/wedding/wedding.js
@@ -1,10 +1,7 @@
 import ProjectEmbedCard from "../projects/projectCards/templates/cardEmbed"
 import React from "react"
 import '../projects/projectCards/templates/card.css'
-import "https://unpkg.com/@ungap/custom-elements-builtin"
-import "https://unpkg.com/x-frame-bypass"
 
-import { Embed } from 'semantic-ui-react'
 
 class Wedding extends React.Component {
 
@@ -12,7 +9,7 @@ class Wedding extends React.Component {
     return (
       
     
-<iframe is="x-frame-bypass" src="https://www.theknot.com/us/jordan-jarvis-and-rebecca-kiser/	" width="200" height="200" frameBorder="0" allowFullScreen ><p>Your browser does not support iframes.</p></iframe>
+<iframe src="https://www.theknot.com/us/jordan-jarvis-and-rebecca-kiser/	" width="200" height="200" frameBorder="0" allowFullScreen ><p>Your browser does not support iframes.</p></iframe>
 )
 
 }
